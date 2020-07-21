4e622d441c6144c6b788457f663f7521acb96fa6
Jude kuti <judekuti@gmail.com>
2020-07-21 02:39:16 -0500

    Import sqlite and barcodescanner in home and inject

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { SQLite, SQLiteObject } from '@ionic-native/sqlite/ngx';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx'
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  plu = '';
+  barcode = '';
+  name = '';
+  description = '';
+  price = null;
+  productnumber = '';
 
-  constructor() {}
+  constructor(
+    private barcodeScanner: BarcodeScanner,
+    private sqlite: SQLite) {}
+  
+   scanBarcode() {
+    this.barcodeScanner.scan().then(barcodeData => {
+      console.log('Barcode data', barcodeData);
+      this.matchProduct(barcodeData.text);
+     }).catch(err => {
+         console.log('Error', err);
+     });
+  }
 
 }
